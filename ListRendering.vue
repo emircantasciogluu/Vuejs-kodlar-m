1752b65827b0d5ba0ff61f130b3fe8553c922236
Emir Can Taşcıoğlu <159278574+emircantasciogluu@users.noreply.github.com>
2024-02-07 18:47:13 +0300

    Create ListRendering.vue

---
+++ ListRendering.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import {ref} from 'vue';
+
+const members = ref(["Emir", "Mehmet", "Ahmet"])
+
+const member = ref({
+  name: "Batu",
+  age: 30,
+  city: 'Rize'
+})
+
+
+</script>
+
+<template>
+  <div> <!--Array-->
+    <ul>
+      <li v-for="member in members" :key="member">
+        {{ member }}
+      </li>
+    </ul>
+  </div>
+
+  <div> <!--Object-->
+    <ul>
+      <li v-for="(key, value) in member" :key="value">
+      {{ value }} - {{ key }}</li>
+    </ul>
+  </div>
+</template>
+
+<style>
+ul{
+  font-size: 30px;
+  margin-top: 100px;
+}
+
+
+</style>
