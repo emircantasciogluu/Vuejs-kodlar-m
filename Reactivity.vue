48cb50f9493d709ea2c686258768dea65a25fea9
emircantasciogluu <159278574+emircantasciogluu@users.noreply.github.com>
2024-02-07 18:04:49 +0300

    Create Reactivity.vue

---
+++ Reactivity.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+import {ref, watch} from 'vue';
+
+const count = ref(0)
+const log = ref<string>()
+
+function updateCount(){
+  count.value++
+}
+
+watch(count, (newValue, oldValue)=>{
+  log.value = `oldValue: ${oldValue} - newValue: ${newValue}`
+})
+
+</script>
+
+<template>
+  <div>
+    <button @click="updateCount">{{ count }}</button>
+    <pre>
+      {{ log }}
+    </pre>
+  </div>
+</template>
+
+<style>
+
+button{
+  font-size: 30px;
+  margin-top: 100px;
+}
+
+</style>
