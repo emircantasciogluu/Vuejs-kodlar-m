4dc1cc15bb1138e00dd763ce028e1a52ff138f70
Emir Can Taşcıoğlu <159278574+emircantasciogluu@users.noreply.github.com>
2024-02-08 16:10:49 +0300

    Create Class and Style Bindings.vue

---
+++ Class and Style Bindings.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const isRed = ref(false)
+
+
+
+</script>
+
+<template>
+  <div class="container">
+    <h1 :class="{ baslik: isRed }">Emir Can Taşcıoğlu</h1>
+    <button @click="isRed = true">change color</button>
+    <button @click="isRed = false">Reset</button>
+  </div>
+</template>
+
+<style>
+.baslik{
+  color: red;
+}
+
+
+
+</style>
