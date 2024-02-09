bd65791674ca266c5632ef75594fe6a8f3b04496
Emir Can Taşcıoğlu <159278574+emircantasciogluu@users.noreply.github.com>
2024-02-09 13:18:25 +0300

    Update Class and Style Bindings.vue

--- Class and Style Bindings.vue
+++ Class and Style Bindings.vue
@@ -2,12 +2,12 @@
 import { ref } from 'vue'
 
 const isRed = ref(false)
-const myStyle = {
+const myStyle = ref({
   color: 'blue',
   background: 'red',
   padding: '12px 24px' 
 }
-
+)
 
 </script>
 
@@ -16,7 +16,7 @@ const myStyle = {
     <h1 :class="{ baslik: isRed }">Emir Can Taşcıoğlu</h1>
     <h1 :style="myStyle">Vue.js Öğreniyorum</h1>
     <button @click="isRed = true">change color</button>
-    <button @click="isRed = false">Reset</button>
+    <button @click=" myStyle.background = 'yellow' ">Change Bg</button>
   </div>
 </template>
 
